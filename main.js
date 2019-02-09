4d62be4a5897b6dfb68ebab257740c1df007a5a6
greatbridf <guobingfang20040616@gmail.com>
2019-02-09 13:01:16 +0800

    Add menu bar Quit application while closing windows

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-const {app, BrowserWindow, clipboard} = require('electron');
+const {app, BrowserWindow, clipboard, Menu} = require('electron');
 const ipc = require('electron').ipcMain;
 const request = require('request');
 
@@ -8,6 +8,79 @@ if (process.argv.indexOf("--debug") !== -1 || process.argv.indexOf("-d") !== -1)
   debug = true;
 
 let window;
+const template = [
+  {
+    label: app.getName(),
+	submenu: [
+	  { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+	]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'pasteandmatchstyle' },
+      { role: 'delete' },
+      { role: 'selectall' },
+      { type: 'separator' },
+      {
+        label: 'Speech',
+        submenu: [
+          { role: 'startspeaking' },
+          { role: 'stopspeaking' }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+	  { role: 'close' },
+	  { role: 'minimize' },
+	  { role: 'zoom' },
+	  { type: 'separator' },
+	  { role: 'front' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://github.com/greatbridf/LiuliElectron') }
+      }
+    ]
+  }
+]
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 function createWindow() {
   window = new BrowserWindow({width: 1280, height: 720});
@@ -15,6 +88,9 @@ function createWindow() {
 }
 
 app.on('ready', createWindow);
+app.on('window-all-closed', ()=> {
+  app.quit()
+})
 
 // Register IPC listeners
 ipc.on("articlesQuery", (event, req) => {
