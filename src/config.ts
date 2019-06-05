52ec06f085fb923031323b40206dbba8fb397981
greatbridf <guobingfang20040616@gmail.com>
2019-06-06 00:06:41 +0800

    fix(config): make debug mode work normally
    use remote 'process' variable

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,23 @@
-const app = require('electron').app ? require('electron').app : require('electron').remote.app
+import * as electron from 'electron'
 import * as path from 'path'
+
+var app: Electron.App
+var currProcess: NodeJS.Process
+if (electron.app) {
+  app = electron.app
+  currProcess = process
+} else {
+  app = electron.remote.app
+  currProcess = electron.remote.process
+}
 
 class Config {
   cdn_addr: string = 'https://interface.greatbridf.top'
   userData: string = app.getPath('userData')
   fontPath: string = path.join(this.userData, 'SourceHanSansSC-Regular.otf')
   fontCssPath: string = path.join(this.userData, 'font.css')
-  debug: boolean = (process.argv.indexOf("--debug") !== -1 || process.argv.indexOf("-d") !== -1)
-  platform: string = process.platform
+  debug: boolean = (currProcess.argv.indexOf("--debug") !== -1 || currProcess.argv.indexOf("-d") !== -1)
+  platform: string = currProcess.platform
 }
 
 export default Config
